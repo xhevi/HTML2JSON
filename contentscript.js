6894a06d9167f42b898af681fbb04498733accc5
xhevi <qafmolla@gmail.com>
2015-04-30 18:00:47 +0200

    working version with pretty print readable JSON

--- contentscript.js
+++ contentscript.js
@@ -1,8 +1,6 @@
-console.log('contentscript.js was run');
-
 var html = $('html')[0].outerHTML;
 html = html.toString();
 //console.log('Source:'+html);
 var actual = html2json(html);
 //console.log('Result:'+JSON.stringify(actual));
-document.write(JSON.stringify(actual));
+document.write('<pre>'+JSON.stringify(actual, null, 2)+'</pre>');
